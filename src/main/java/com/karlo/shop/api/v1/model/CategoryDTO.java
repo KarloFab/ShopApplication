8e4ec7c5d96dcf51140e38d2b8862b2b31365004
KarloFab <fabijanickarlo1@gmail.com>
2019-10-01 22:06:57 +0200

    Added initial shop project

---
+++ src/main/java/com/karlo/shop/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package com.karlo.shop.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/karlo/shop/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.karlo.shop.repository;
+
+import com.karlo.shop.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
