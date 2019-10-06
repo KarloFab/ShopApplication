49679dfbbab2968bcd1de30544294d21d83accce
KarloFab <fabijanickarlo1@gmail.com>
2019-10-06 19:09:50 +0200

    Created CategoryMapper test

--- src/main/java/com/karlo/shop/api/v1/model/CategoryDTO.java
+++ src/main/java/com/karlo/shop/api/v1/model/CategoryDTO.java
@@ -1,5 +1,8 @@
 package com.karlo.shop.api.v1.model;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
--- src/test/java/com/karlo/shop/api/v1/mapper/CategoryMapperTest.java
+++ src/test/java/com/karlo/shop/api/v1/mapper/CategoryMapperTest.java
@@ -1,4 +1,28 @@
 package com.karlo.shop.api.v1.mapper;
 
+
+import com.karlo.shop.api.v1.model.CategoryDTO;
+import com.karlo.shop.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CategoryMapperTest {
+
+    public static final String NAME = "Joe";
+    public static final long ID = 1L;
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDTO() throws Exception {
+
+        Category category = new Category();
+        category.setName(NAME);
+        category.setId(ID);
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        assertEquals(Long.valueOf(1L), categoryDTO.getId());
+        assertEquals("Joe", categoryDTO.getName());
+    }
 }
