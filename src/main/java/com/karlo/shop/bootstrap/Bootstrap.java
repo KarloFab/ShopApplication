58933e47a3e97dfd6dabac9b935b91eb000651a7
KarloFab <fabijanickarlo1@gmail.com>
2019-10-07 19:55:02 +0200

    Added boostraping Customers

--- src/main/java/com/karlo/shop/bootstrap/Bootstrap.java
+++ src/main/java/com/karlo/shop/bootstrap/Bootstrap.java
@@ -1,19 +1,26 @@
 package com.karlo.shop.bootstrap;
 
 import com.karlo.shop.domain.Category;
+import com.karlo.shop.domain.Customer;
 import com.karlo.shop.repository.CategoryRepository;
+import com.karlo.shop.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class Bootstrap implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        loadCustomers();
+
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -26,5 +33,20 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(fruits);
         categoryRepository.save(vegetables);
         categoryRepository.save(socks);
+    }
+
+    private void loadCustomers() {
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("John");
+        customer.setLastName("Johhny");
+        customerRepository.save(customer);
+
+        Customer customer2 = new Customer();
+        customer2.setId(2L);
+        customer2.setFirstName("Bob");
+        customer2.setLastName("Bobby");
+        customerRepository.save(customer2);
     }
 }
--- src/main/java/com/karlo/shop/repository/CustomerRepository.java
+++ src/main/java/com/karlo/shop/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.karlo.shop.repository;
 import com.karlo.shop.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Long, Customer> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
