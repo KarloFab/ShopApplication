7c8167e43d8c172bf57a910a93e3f3eac52f5948
KarloFab <fabijanickarlo1@gmail.com>
2019-10-07 19:49:59 +0200

    Added customer domain model and DTO

---
+++ src/main/java/com/karlo/shop/api/v1/model/CustomerDTO.java
@@ -0,0 +1,10 @@
+package com.karlo.shop.api.v1.model;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+    private String firstName;
+    private String lastName;
+    private String customerUrl;
+}
