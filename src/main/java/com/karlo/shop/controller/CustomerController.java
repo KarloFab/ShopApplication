190ed48bd72324990b3748babecc189e43507975
KarloFab <fabijanickarlo1@gmail.com>
2019-10-07 20:19:23 +0200

    Added test for CustomerController

--- src/main/java/com/karlo/shop/controller/CustomerController.java
+++ src/main/java/com/karlo/shop/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,7 +27,7 @@ public class CustomerController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<CustomerDTO> getCustomerById(Long id){
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id){
         return new ResponseEntity<>(
                 customerService.getCustomerById(id), HttpStatus.OK);
     }
---
+++ src/test/java/com/karlo/shop/controller/CustomerControllerTest.java
@@ -0,0 +1,81 @@
+package com.karlo.shop.controller;
+
+import com.karlo.shop.api.v1.model.CustomerDTO;
+import com.karlo.shop.service.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class CustomerControllerTest {
+
+    public static final Long ID = 1L;
+    public static final String NAME = "Joe";
+    public static final String LAST_NAME = "Joey";
+
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    public void getAllCustomers() throws Exception {
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName(NAME);
+        customerDTO.setLastName(LAST_NAME);
+
+        CustomerDTO customerDTO2 = new CustomerDTO();
+        customerDTO2.setFirstName("Bob");
+        customerDTO2.setLastName("Bobby");
+
+        List<CustomerDTO> customers = Arrays.asList(customerDTO, customerDTO2);
+
+        when(customerService.getAllCustomers()).thenReturn(customers);
+
+        mockMvc.perform(get("/api/v1/customers/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customers",hasSize(2)));
+
+    }
+
+    @Test
+    public void getCustomerById() throws Exception {
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName(NAME);
+        customerDTO.setLastName(LAST_NAME);
+
+        when(customerService.getCustomerById(anyLong())).thenReturn(customerDTO);
+
+        mockMvc.perform(get("/api/v1/customers/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName",equalTo(NAME)))
+                .andExpect(jsonPath("$.lastName",equalTo(LAST_NAME)));
+    }
+}
