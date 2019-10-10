ead5df530e201f85c809efe9dd73d23df9ea08c1
KarloFab <fabijanickarlo1@gmail.com>
2019-10-10 19:09:43 +0200

    Added Vendor model and DTO

---
+++ src/main/java/com/karlo/shop/api/v1/model/VendorListDTO.java
@@ -0,0 +1,13 @@
+package com.karlo.shop.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class VendorListDTO {
+    private List<VendorDTO> vendors;
+
+}
